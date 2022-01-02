7c82439bffdb6c91ef5d7e10d5327da14789b401
Jakub Švarc <jakubsvarc@protonmail.com>
2022-01-02 12:59:50 +0100

    feat: added arguments handling, small comments refactor

--- post-installation-script.sh
+++ post-installation-script.sh
@@ -2,13 +2,37 @@
 
 # SPDX-License-Identifier: MIT
 
+# Arguments handling
+
+redirectLong=/dev/null
+redirectShort=/dev/stdout
+
+while getopts ":h" argument; do
+    case $argument in
+        s)  # Silent
+            redirectLong=/dev/null
+            redirectShort=/dev/null;;
+        n)  # Normal
+            redirectLong=/dev/null
+            redirectShort=/dev/stdout;;
+        v)  # Vorbose
+            redirectLong=/dev/stdout
+            redirectShort=/dev/stdout;;
+        \?)
+            echo "Error: Invalid argument"
+            exit;;
+    esac
+done
+
 # User defined variables
 
-## Applications installed by APT
-APT="terminator openjdk-11-jdk brave-browser codium thunderbird signal-desktop transmission-gtk libreoffice gimp rhythmbox steam"
+## Applications installed by APT (separated by spaces)
 
-## Applications installed by Flatpak
-FLATPAK="com.gitlab.davem.ClamTk cc.arduino.arduinoide org.apache.netbeans org.kicad.KiCad io.freetubeapp.FreeTube com.prusa3d.PrusaSlicer"
+apt="terminator openjdk-13-jdk brave-browser codium thunderbird signal-desktop transmission-gtk libreoffice gimp rhythmbox steam"
+
+## Applications installed by Flatpak (separated by spaces)
+
+flatpak="com.gitlab.davem.ClamTk cc.arduino.arduinoide org.apache.netbeans org.kicad.KiCad io.freetubeapp.FreeTube com.prusa3d.PrusaSlicer"
 
 ## Groups where user should be added (separated by commas)
 
@@ -16,45 +40,50 @@ groups="dialout"
 
 # Script preset
 
-echo "Post-Installation Script\n------------------------"
+echo "Post-Installation Script\n------------------------" > redirectShort
 
-## Initial SW update and installation
+# Installation
 
-echo -n "Starting software update..."
-apt-get update > /dev/null && apt-get -y upgrade > /dev/null
-echo " DONE"
-echo -n "Starting installation of support SW..."
-apt-get install apt-transport-https wget > /dev/null
-echo " DONE"
+## Initial update and support SW installation
 
-# Preinstallation setup
+echo -n "Starting software update..." > redirectShort
+apt-get update > redirectLong && apt-get -y upgrade > redirectLong
+echo " DONE" > redirectShort
+echo -n "Starting installation of support SW..." > redirectShort
+apt-get install apt-transport-https wget > redirectLong
+echo " DONE" > redirectShort
 
-## Flatpak
-echo "Checking Flatpak presence"
+## Preinstallation setup
+
+### Flatpak setup
+
+echo "Checking Flatpak presence..." > redirectShort
 
 if ! flatpak --version > /dev/null
 then
-    echo -n "Flatpak is not installed. Starting installation..."
-    apt-get install flatpak > /dev/null
+    echo -n "Flatpak is not installed. Starting installation..." > redirectShort
+    apt-get install flatpak > redirectLong
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    echo "DONE\nFlatpak is succesfully installed. Now you must restart the computer."
+    echo " DONE\nFlatpak is succesfully installed. Now you must restart the computer." > redirectShort
     exit 0
 else
-    echo "Flatpak is already installed"
+    echo "Flatpak is already installed" > redirectShort
 fi
 
-## Brave
-wget -qO /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
+### Brave repository setup
 
-## VSCodium
+wget -qO /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list > redirectLong
+
+### VSCodium repository setup
+
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+    | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list > /dev/null
+    | tee /etc/apt/sources.list.d/vscodium.list > redirectLong
 
-### Signal
+### Signal repository setup
 
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > redirectLong
@@ -62,41 +91,44 @@ cat signal-desktop-keyring.gpg | tee -a /usr/share/keyrings/signal-desktop-keyri
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
 tee -a /etc/apt/sources.list.d/signal-xenial.list > redirectLong
 
-# User programs installation
 
-## APT
-echo "Starting installation of following programs (by APT): $APT"
-apt-get -y install $APT > /dev/null
-echo "DONE"
+## User programs installation
 
-## Flatpak
-echo "Starting installation of following programs (by Flatpak): $FLATPAK"
-flatpak install -y --noninteractive flathub $FLATPAK > /dev/null
-echo "DONE"
+### APT installation
+
+echo "Starting installation of following programs (by APT): $apt" > redirectShort
+apt-get update > redirectLong && apt-get -y install $apt > redirectLong
+echo "DONE" > redirectShort
+
+### Flatpak installation
+
+echo "Starting installation of following programs (by Flatpak): $flatpak" > redirectShort
+flatpak install -y --noninteractive flathub $flatpak > redirectLong
+echo "DONE" > redirectShort
 
 # Settings
 
 ## .bashrc
 
-echo -n "Setting default apps..."
+echo -n "Setting default apps..." > redirectShort
 echo "export BROWSER=/usr/bin/brave-browser" >> ~/.bashrc
-echo " DONE"
+echo " DONE" > redirectShort
 
-echo -n "Setting shell prompt..."
+echo -n "Setting shell prompt..." > redirectShort
 echo "export PS1=\"\[\033[38;5;76m\]\[$(tput bold)\]┌─(\[$(tput sgr0)\]\[\033[38;5;255m\]\[$(tput bold)\]\u\[$(tput sgr0)\]\[\033[38;5;76m\]\[$(tput bold)\]@\[$(tput sgr0)\]\[\033[38;5;255m\]\[$(tput bold)\]\H\[$(tput sgr0)\]\[\033[38;5;76m\]\[$(tput bold)\])-[\[$(tput sgr0)\]\[\033[38;5;255m\]\[$(tput bold)\]\w\[$(tput sgr0)\]\[\033[38;5;76m\]\[$(tput bold)\]]\n└──\\$\[$(tput sgr0)\] \"" >> ~/.bashrc
-echo " DONE"
+echo " DONE" > redirectShort
 
 ## Groups
 
-echo "Adding user to following groups: $groups"
+echo "Adding user to following groups: $groups" > redirectShort
 usermod -aG $groups $(whoami)
-echo "DONE"
+echo "DONE" > redirectShort
 
 ## Applications config
 
 ### VSCodium
 
-echo -n "Creating product.json file..."
+echo -n "Creating product.json file..." > redirectShort
 echo "{
   \"extensionsGallery\": {
     \"serviceUrl\": \"https://marketplace.visualstudio.com/_apis/public/gallery\",
@@ -106,4 +138,4 @@ echo "{
     \"recommendationsUrl\": \"\"
   }
 }" > ~/.config/VSCodium/product.json
-echo " DONE"
+echo " DONE" > redirectShort
