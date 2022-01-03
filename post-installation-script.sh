c507d9518a851ef471aa1a58cba926b461185aea
Jakub Švarc <jakubsvarc@protonmail.com>
2022-01-03 01:30:11 +0100

    fix: fixed verbose output

--- post-installation-script.sh
+++ post-installation-script.sh
@@ -41,12 +41,12 @@ done
 echo -e "Post-Installation Script\n------------------------" > $redirectShort
 
 ## Initial update and support SW installation
-echo -n "Starting software update..." > $redirectShort
+echo "Starting software update..." > $redirectShort
 apt-get update > $redirectLong && apt-get -y upgrade > $redirectLong
-echo " DONE" > $redirectShort
-echo -n "Starting installation of support SW..." > $redirectShort
+echo "DONE" > $redirectShort
+echo "Starting installation of support SW..." > $redirectShort
 apt-get -y install apt-transport-https wget > $redirectLong
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 ## User programs preinstallation setup
 ### Flatpak setup
@@ -54,63 +54,63 @@ echo "Checking Flatpak presence..." > $redirectShort
 
 if ! flatpak --version > /dev/null 2> /dev/null
 then
-    echo -n "Flatpak is not installed. Starting installation..." > $redirectShort
+    echo "Flatpak is not installed. Starting installation..." > $redirectShort
     apt-get -y install flatpak > $redirectLong
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    echo -e " DONE\nFlatpak is succesfully installed. Now you must restart the computer."
+    echo -e "DONE\nFlatpak is succesfully installed. Now you must restart the computer."
     exit 0
 else
     echo "Flatpak is already installed" > $redirectShort
 fi
 
 ### Brave repository setup
-echo -n "Adding Brave repository..." > $redirectShort
+echo "Adding Brave repository..." > $redirectShort
 wget -qO /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list > $redirectLong
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 ### VSCodium repository setup
-echo -n "Adding VSCodium repository..." > $redirectShort
+echo "Adding VSCodium repository..." > $redirectShort
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor > /usr/share/keyrings/vscodium-archive-keyring.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | tee /etc/apt/sources.list.d/vscodium.list > $redirectLong
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 ### Signal repository setup
-echo -n "Adding Signal repository..." > $redirectShort
+echo "Adding Signal repository..." > $redirectShort
 wget -qO - https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > /usr/share/keyrings/signal-desktop-keyring.gpg
 
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
 tee /etc/apt/sources.list.d/signal-xenial.list > $redirectLong
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 
 ## User programs installation
 ### APT programs installation
-echo -n "Starting installation of following programs (by APT): $apt..." > $redirectShort
+echo "Starting installation of following programs (by APT): $apt..." > $redirectShort
 apt-get update > $redirectLong && apt-get -y install $apt > $redirectLong
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 ### Flatpak programs installation
-echo -n "Starting installation of following programs (by Flatpak): $flatpak..." > $redirectShort
+echo "Starting installation of following programs (by Flatpak): $flatpak..." > $redirectShort
 flatpak install -y --noninteractive flathub $flatpak > $redirectLong
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 # Settings
 ## Adding records to .bashrc file
-echo -n "Setting default apps..." > $redirectShort
+echo "Setting default apps..." > $redirectShort
 echo "export BROWSER=/usr/bin/brave-browser" >> "/home/$SUDO_USER/.bashrc"
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
-echo -n "Setting shell prompt..." > $redirectShort
+echo "Setting shell prompt..." > $redirectShort
 echo "export PS1=\"\[\033[38;5;76m\]\[$(tput bold)\]┌─(\[$(tput sgr0)\]\[\033[38;5;255m\]\[$(tput bold)\]\u\[$(tput sgr0)\]\[\033[38;5;76m\]\[$(tput bold)\]@\[$(tput sgr0)\]\[\033[38;5;255m\]\[$(tput bold)\]\H\[$(tput sgr0)\]\[\033[38;5;76m\]\[$(tput bold)\])-[\[$(tput sgr0)\]\[\033[38;5;255m\]\[$(tput bold)\]\w\[$(tput sgr0)\]\[\033[38;5;76m\]\[$(tput bold)\]]\n└──\\$\[$(tput sgr0)\] \"" >> "/home/$SUDO_USER/.bashrc"
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 ## Adding user to groups
-echo -n "Adding user to following groups: $groups..." > $redirectShort
+echo "Adding user to following groups: $groups..." > $redirectShort
 usermod -aG $groups $SUDO_USER
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
 
 ## Applications config
 ### VSCodium
@@ -118,17 +118,17 @@ if [[ ! -d "/home/$SUDO_USER/.config/VSCodium" ]]
 then
     if [[ ! -d "/home/$SUDO_USER/.config" ]]
     then
-        echo -n "Creating \"~/.config\" folder..." > $redirectShort
+        echo "Creating \"~/.config\" folder..." > $redirectShort
         mkdir "/home/$SUDO_USER/.config/VSCodium"
-        echo " DONE" > $redirectShort
+        echo "DONE" > $redirectShort
     fi
 
-    echo -n "Creating \"~/.config/VSCodium\" folder..." > $redirectShort
+    echo "Creating \"~/.config/VSCodium\" folder..." > $redirectShort
     mkdir "/home/$SUDO_USER/.config/VSCodium"
-    echo " DONE" > $redirectShort
+    echo "DONE" > $redirectShort
 fi
 
-echo -n "Creating product.json file..." > $redirectShort
+echo "Creating product.json file..." > $redirectShort
 echo "{
   \"extensionsGallery\": {
     \"serviceUrl\": \"https://marketplace.visualstudio.com/_apis/public/gallery\",
@@ -138,4 +138,4 @@ echo "{
     \"recommendationsUrl\": \"\"
   }
 }" > "/home/$SUDO_USER/.config/VSCodium/product.json"
-echo " DONE" > $redirectShort
+echo "DONE" > $redirectShort
